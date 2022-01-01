c0f313414448a4d1f3eeb081c62d7f9a679019b6
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2022-01-01 04:05:57 -0800

    Adding:welcome msg of bookstore

---
+++ src/Pages/Signup/Registration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Registration() {
+    return (
+        <div>
+            <h1>welcome to bookstore</h1>
+        </div>
+    )
+}
+
+export default Registration
