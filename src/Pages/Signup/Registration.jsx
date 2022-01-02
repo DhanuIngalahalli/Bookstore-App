e563a96926cc61521a6840a8f59268dcc44f958a
DhanuIngalahalli <dhanashree.ingalahalli5@gmail.com>
2022-01-01 20:59:48 -0800

    Adding:pushing code for signup

--- src/Pages/Signup/Registration.jsx
+++ src/Pages/Signup/Registration.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './Registration.css';
 
 function Registration() {
     return (
-        <div>
-            <h1>welcome to bookstore</h1>
+  <div className="signup-form">
+          <input className="firstname"  type="text" placeholder="Full Name" name="fullName" />
+          <input className="email-id"   type="text" placeholder="Email"     name="email"/>
+          <input className="pwd"        type="text" placeholder="Password"  name="password"/>
+          <input className="phone-num" type="text"  placeholder="Mobile Number"  name="phone" />
+          <button className="button" >Signup</button>
         </div>
     )
 }
